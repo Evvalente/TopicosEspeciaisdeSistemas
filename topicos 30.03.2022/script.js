da4a09db213463dee7925002b12764f669c72156
Evvalente <evvalente44@gmail.com>
2022-03-30 21:48:16 -0300

    Add files via upload
    Aula do dia 30.03.22

---
+++ topicos 30.03.2022/script.js
@@ -0,0 +1,31 @@
+//function criarParagrafo() {
+   // let para = document.createElement('p');
+   // para.textContent = 'Você clicou no botão!';
+   // document.body.appendChild(para);
+//}
+//const botoes = document.querySelectorAll('button');
+//for(var i = 0; i < botoes.length ; i++) {
+  //  botoes[i].addEventListener('click', criarParagrafo);
+//}
+
+function contador(){
+    let para = document.createElement('p');
+    para.textContent = '1,2,3,4,5,6,7,8,9,10';
+    document.body.appendChild(para);  
+}
+
+function segundobotao(){
+    let para = document.createElement('p');
+    para.textContent = 'Também conhecido como dívida técnica, o débito técnico é um conceito no desenvolvimento de software utilizado para representar o custo implícito de uma implementação'; 
+    document.body.appendChild(para);
+}
+
+function sonar(){
+    window.open("https://sonarcloud.io", "_blank");
+}
+
+function jenkins(){
+    window.open("https://www.gocache.com.br/dicas/o-que-e-jenkins-para-iniciantes/," "_blanck");
+}
+ 
+
